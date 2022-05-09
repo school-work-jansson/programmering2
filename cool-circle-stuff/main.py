d814a69f6049c2778361ce1064ed45721637ea1c
Linus Jansson <linus.jansson2000@gmail.com>
2022-05-09 15:28:09 +0200

    did tweaking

--- cool-circle-stuff/main.py
+++ cool-circle-stuff/main.py
@@ -10,7 +10,7 @@ class SpecialCircle():
         self.xPos, self.yPos = randrange(1, 400), 0 
         self.r = randrange(1, 10)
         self.dx, self.dy = 1, randrange(1, 100) / 100   
-        self.opacity = randrange(0, 50)
+        self.opacity = randrange(0, 75)
         
         self.drawing = Circle(self.xPos, self.yPos, self.r, opacity=self.opacity, fill="white")
     
@@ -28,7 +28,7 @@ def onStep():
         circle.move()
     
 def main():
-    for i in range(250):
+    for i in range(500):
         app.circles.append(SpecialCircle())
     
 
