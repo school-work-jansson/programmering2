65cdf7ec68d3da9b545b43b6fdd9ca3f365a6bf7
Linus Jansson <linus.jansson2000@gmail.com>
2022-05-09 15:17:57 +0200

    something circle stuff

---
+++ cool-circle-stuff/main.py
@@ -0,0 +1,33 @@
+# Creating on Github
+# github.com/school-work-jansson/programmering2
+Label("CODE IS ON GITHUB", 200, 200, size=25)
+
+class SpecialCircle():
+    def __init__(self):
+        self.xPos, self.yPos = randrange(1, 400), 0 
+        self.r = randrange(1, 10)
+        self.dx, self.dy = 1, randrange(1, 100) / 100   
+        self.opacity = randrange(0, 50)
+        
+        self.drawing = Circle(self.xPos, self.yPos, self.r, opacity=self.opacity)
+    
+    def move(self):
+        if self.drawing.centerY >= 400:
+            self.drawing.centerY = 0
+            self.dy = randrange(1, 100) / 100
+        else:
+            self.drawing.centerY += self.dy
+
+app.circles = []
+
+def onStep():
+    for circle in app.circles:
+        circle.move()
+    
+def main():
+    for i in range(250):
+        app.circles.append(SpecialCircle())
+    
+
+main()
+    
