2e831db8c427e1be9e539a9c7d9077e9c464a39f
Linus Jansson <linus.jansson2000@gmail.com>
2022-05-10 13:00:33 +0200

    timeout if enemey hits one another

--- final_project/main.py
+++ final_project/main.py
@@ -1,6 +1,8 @@
 from cmu_graphics import *
-
+import time
 # Fill me in!
+
+
 
 class SpecialObject(object):
     def __init__(self, w=None, h=None, xPos=None, yPos=None, drawing=None):
@@ -12,16 +14,19 @@ class SpecialObject(object):
         self.xPos = xPos if xPos is not None else randrange(0 + self.w, 400 - self.w)
         self.yPos = yPos if yPos is not None else randrange(0 + self.h, 400 - self.h)
 
-        self.speedX = 2.5
-        self.speedY = 2.5
+        self.speedX = 1.5
+        self.speedY = 1.5
 
         self.drawing = drawing
+
+        self.angle = 0
 
 
     def update(self):
         # Update screen
         self.drawing.centerX = self.xPos
         self.drawing.centerY = self.yPos
+        self.drawing.rotateAngle = self.angle
 
 
     def distanceTo(self, obj):
@@ -33,7 +38,7 @@ class SpecialObject(object):
         
         return x, y, distance
         
-        
+
     def directionToObject(self, obj):
         # Calculating the direction to a object 
         # Used to move enemey towards player
@@ -46,6 +51,28 @@ class SpecialObject(object):
         direction = (x, y)
         
         return direction
+
+
+class Objective(SpecialObject):
+    def __init__(self):
+        self.xPos = randrange(0, 400)
+        self.yPos = randrange(0, 400)
+        self.r = 12.5
+
+        drawing = Group(
+            Star(self.xPos, self.yPos, self.r, 5, fill="gold")
+        )
+        
+        super().__init__(drawing=drawing)
+
+        self.update()
+    
+    def newPos(self):
+        self.xPos = randrange(0, 400)
+        self.yPos = randrange(0, 400)
+
+        self.update()
+
 
 class Player(SpecialObject):
     def __init__(self, startX=None, startY=None):
@@ -65,6 +92,9 @@ class Player(SpecialObject):
         super().__init__(self.w, self.h, xPos=startX, yPos=startY, drawing=drawing)
 
         self.update()
+
+        self.speedX = 2.5
+        self.speedY = 2.5
             
         
             
@@ -98,9 +128,10 @@ class Enemey(SpecialObject):
         # velocity
         self.dx = 1
         self.dy = 1
+        self.last_hit = time.time()
         
         drawing = Group(
-            Rect(self.xPos, self.yPos, self.w, self.h, fill="red")
+            Rect(self.xPos, self.yPos, self.w, self.h, fill="red", border="darkred", )
             )
 
         super().__init__(self.w, self.h, xPos=startX, yPos=startY, drawing=drawing)
@@ -111,37 +142,52 @@ class Enemey(SpecialObject):
     def move(self, distRef, enemies):
 
         distX, distY = self.directionToObject(distRef)
+        self.angle += 5
+
 
         # Corner colission
         if self.xPos > 400 - self.w:
             self.speedX *= -1
+            self.angle += 45
         
         elif self.xPos < 0 + self.w:
             self.speedX *= -1
+            self.angle += 45
             
         if self.yPos  > 400 - self.h:
             self.speedY *= -1
+            self.angle += 45
             
         elif self.yPos  < 0 + self.h:
             self.speedY *= -1
+            self.angle += 45
 
-        self.xPos += self.speedX * distX
-        self.yPos += self.speedY * distY
 
+
+        
         # make sure not to collide with other enemies
         # Works but is buggy
         for enemy in enemies:
+            # Dont check for it self
             if enemy.drawing != self.drawing:
-                x_dist, y_dist, _ = self.distanceTo(enemy)
+                if enemy.drawing.hitsShape(self.drawing):
+                    self.speedY *= -1
+                    self.speedX *= -1
 
-                while enemy.drawing.hitsShape(self.drawing):
-                    self.xPos += x_dist
-                    self.yPos += y_dist
-            
-                    self.update()
+                    # Add delay from moving to player if they collide
+                    self.last_hit = time.time()
                 
                 # Print distance between the enemy
                 # print(x_dist, y_dist)
+
+        # Prevent enemey from hitting player if it has collided
+        if time.time() - self.last_hit <= 2:
+            # print("hit timeout", time.time() - self.last_hit)
+            self.xPos += self.speedX
+            self.yPos += self.speedY
+        else:
+            self.xPos += self.speedX * distX
+            self.yPos += self.speedY * distY
 
         self.update()
 
@@ -174,6 +220,8 @@ def onStep():
 
 # Initial start create player instance and enemies
 def main():
+    app.background = rgb(15, 14, 14)
+    app.objective = Objective()
     app.player = Player(200, 200)
     app.enemies = []
 
